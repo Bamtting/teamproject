e20ef4d4f519a68a6bb699ff8a5a7c12824517b5
dnjepddl <dnjepddl@gmail.com>
2023-08-28 12:39:57 +0900

    feat : 조장 merge완료

---
+++ js/kwaw.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+  var visualswiper = new Swiper(".visual-mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
