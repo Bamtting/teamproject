ba15df0349d5ab64a19fa8d7d4b1c7752685ae3a
Bamtting <ekekektmdals@gmail.com>
2023-08-29 15:13:03 +0900

    feat : 작업진행중
    video 파트 동영상 및 기능 추가

--- js/kwak.js
+++ js/kwak.js
@@ -9,4 +9,22 @@ window.addEventListener("load", function () {
       prevEl: ".swiper-button-prev",
     },
   });
+  // 비디오 제어
+  const videoPoster = document.querySelector(".videos-poster");
+  const videoBtn = document.querySelector(".video-btn");
+  const videoBox = document.querySelector(".video-box");
+  // 비디오 파일
+  const videos = document.querySelector("video");
+  videos.currentTime = 0;
+  videos.pause();
+
+  videoBtn.addEventListener("click", function () {
+    // 비디오 효과 div 숨기기
+    videoPoster.classList.add("video-box-hide");
+    // 비디오 div 보여주기
+    videoBox.classList.add("video-active");
+    // 비디오 실행하기
+    videos.currentTime = 0;
+    videos.play();
+  });
 });
