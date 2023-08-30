3d501455746f97ff6f7c34aa6c116ffbf821eefe
dnjepddl <dnjepddl@gmail.com>
2023-08-30 19:08:40 +0900

    feat : 다시 수정

--- js/woo.js
+++ js/woo.js
@@ -7,34 +7,45 @@ window.addEventListener("load", function () {
     grabCursor: true,
   });
   // space
-  
-const spaceimage1 = document.getElementById("image1");
-const spaceimage2 = document.getElementById("image2");
 
-const spacebutton1 = document.getElementById("button1");
-const spacebutton2 = document.getElementById("button2");
+  const spaceimage1 = document.getElementById("image1");
+  const spaceimage2 = document.getElementById("image2");
 
-spacebutton1.addEventListener("click", () => {
+  const spacebutton1 = document.getElementById("button1");
+  const spacebutton2 = document.getElementById("button2");
+
+  spacebutton1.addEventListener("click", () => {
     spaceimage1.style.display = "block";
     spaceimage2.style.display = "none";
-});
+  });
 
-spacebutton2.addEventListener("click", () => {
+  spacebutton2.addEventListener("click", () => {
     spaceimage1.style.display = "none";
     spaceimage2.style.display = "block";
-});
-// highlight
-var highlightswiper = new Swiper(".highlight-mySwiper", {
-  effect: "cube",
-  grabCursor: true,
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+  });
+  // ===========================================
+  // highlight
+  var highlightswiper = new Swiper(".highlightswiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  // highlighttxth3.addEventListener("click", function () {
+  //   highlighttxtp.style.display = "block";
+  // });
+     // 클릭 이벤트 핸들러 함수
+     function addClassToElements() {
+      // 모든 클래스를 추가할 대상 요소들을 선택합니다.
+      var elements = document.querySelectorAll('.performance-subtitle');
+
+      // 요소들에 클래스를 추가합니다.
+      elements.forEach(function(element) {
+          element.classList.add('active');
+      });
+  }
+
+  // 클릭 이벤트를 감지하고 함수를 실행합니다.
+  document.addEventListener('click', addClassToElements);
 });
