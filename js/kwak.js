5d9915210e019bcc73c06e6df0c06d26a0c346d0
Bamtting <ekekektmdals@gmail.com>
2023-09-01 16:40:38 +0900

    feat: 작업 진행중
    시승 신청 페이지 작업진행중
    
    video 파트 옵션 추가

--- js/kwak.js
+++ js/kwak.js
@@ -55,6 +55,7 @@ window.addEventListener("load", function () {
   const videoPoster = document.querySelector(".videos-poster");
   const videoBtn = document.querySelector(".video-btn");
   const videoBox = document.querySelector(".video-box");
+  const videobc = this.document.querySelector(".videos");
   // 비디오 파일
   const videos = document.querySelector("video");
   videos.currentTime = 0;
@@ -62,6 +63,7 @@ window.addEventListener("load", function () {
 
   videoBtn.addEventListener("click", function () {
     // 비디오 효과 div 숨기기
+    videobc.style.backgroundColor = "#000";
     videoPoster.classList.add("video-box-hide");
     // 비디오 div 보여주기
     videoBox.classList.add("video-active");
